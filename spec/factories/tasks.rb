db1fe18f511d1b5943966b11bd39c99861850c37
かまてぃさんぽ <75981832+hidenari-kamata@users.noreply.github.com>
2021-03-30 12:40:05 +0900

    Revert "04_未完成のテストコード修正"

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,15 +1,9 @@
 FactoryBot.define do
   factory :task do
     title { 'Task' }
-    status { :todo }
+    status { rand(2) }
     from = Date.parse("2019/08/01")
     to   = Date.parse("2019/12/31")
     deadline { Random.rand(from..to) }
-    association :project
-
-    trait :done do
-      status { :done }
-      completion_date { Time.current.yesterday }
-    end
   end
 end
